jwendell/maistra-api

MAISTRA-2330: bring remaining api's from istio

client/informers/externalversions/core/v1alpha1/interface.go
@@ -24,6 +24,8 @@ import (
 type Interface interface {
 	// MeshFederations returns a MeshFederationInformer.
 	MeshFederations() MeshFederationInformer
+	// ServiceExportses returns a ServiceExportsInformer.
+	ServiceExportses() ServiceExportsInformer
 	// ServiceMeshExtensions returns a ServiceMeshExtensionInformer.
 	ServiceMeshExtensions() ServiceMeshExtensionInformer
 }
@@ -42,6 +44,11 @@ func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakList
 // MeshFederations returns a MeshFederationInformer.
 func (v *version) MeshFederations() MeshFederationInformer {
 	return &meshFederationInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
+}
+
+// ServiceExportses returns a ServiceExportsInformer.
+func (v *version) ServiceExportses() ServiceExportsInformer {
+	return &serviceExportsInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
 }
 
 // ServiceMeshExtensions returns a ServiceMeshExtensionInformer.

client/informers/externalversions/core/v1alpha1/serviceexports.go
@@ -1,0 +1,88 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by main. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	"context"
+	time "time"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	watch "k8s.io/apimachinery/pkg/watch"
+	cache "k8s.io/client-go/tools/cache"
+	internalinterfaces "maistra.io/api/client/informers/externalversions/internalinterfaces"
+	v1alpha1 "maistra.io/api/client/listers/core/v1alpha1"
+	versioned "maistra.io/api/client/versioned"
+	corev1alpha1 "maistra.io/api/core/v1alpha1"
+)
+
+// ServiceExportsInformer provides access to a shared informer and lister for
+// ServiceExportses.
+type ServiceExportsInformer interface {
+	Informer() cache.SharedIndexInformer
+	Lister() v1alpha1.ServiceExportsLister
+}
+
+type serviceExportsInformer struct {
+	factory          internalinterfaces.SharedInformerFactory
+	tweakListOptions internalinterfaces.TweakListOptionsFunc
+	namespace        string
+}
+
+// NewServiceExportsInformer constructs a new informer for ServiceExports type.
+// Always prefer using an informer factory to get a shared informer instead of getting an independent
+// one. This reduces memory footprint and number of connections to the server.
+func NewServiceExportsInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers) cache.SharedIndexInformer {
+	return NewFilteredServiceExportsInformer(client, namespace, resyncPeriod, indexers, nil)
+}
+
+// NewFilteredServiceExportsInformer constructs a new informer for ServiceExports type.
+// Always prefer using an informer factory to get a shared informer instead of getting an independent
+// one. This reduces memory footprint and number of connections to the server.
+func NewFilteredServiceExportsInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().ServiceExportses(namespace).List(context.TODO(), options)
+			},
+			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().ServiceExportses(namespace).Watch(context.TODO(), options)
+			},
+		},
+		&corev1alpha1.ServiceExports{},
+		resyncPeriod,
+		indexers,
+	)
+}
+
+func (f *serviceExportsInformer) defaultInformer(client versioned.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
+	return NewFilteredServiceExportsInformer(client, f.namespace, resyncPeriod, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc}, f.tweakListOptions)
+}
+
+func (f *serviceExportsInformer) Informer() cache.SharedIndexInformer {
+	return f.factory.InformerFor(&corev1alpha1.ServiceExports{}, f.defaultInformer)
+}
+
+func (f *serviceExportsInformer) Lister() v1alpha1.ServiceExportsLister {
+	return v1alpha1.NewServiceExportsLister(f.Informer().GetIndexer())
+}

client/informers/externalversions/generic.go
@@ -65,6 +65,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		// Group=core, Version=v1alpha1
 	case v1alpha1.SchemeGroupVersion.WithResource("meshfederations"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().MeshFederations().Informer()}, nil
+	case v1alpha1.SchemeGroupVersion.WithResource("serviceexportses"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().ServiceExportses().Informer()}, nil
 	case v1alpha1.SchemeGroupVersion.WithResource("servicemeshextensions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().ServiceMeshExtensions().Informer()}, nil
 

client/listers/core/v1alpha1/serviceexports.go
@@ -1,0 +1,97 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by main. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/tools/cache"
+	v1alpha1 "maistra.io/api/core/v1alpha1"
+)
+
+// ServiceExportsLister helps list ServiceExportses.
+// All objects returned here must be treated as read-only.
+type ServiceExportsLister interface {
+	// List lists all ServiceExportses in the indexer.
+	// Objects returned here must be treated as read-only.
+	List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error)
+	// ServiceExportses returns an object that can list and get ServiceExportses.
+	ServiceExportses(namespace string) ServiceExportsNamespaceLister
+	ServiceExportsListerExpansion
+}
+
+// serviceExportsLister implements the ServiceExportsLister interface.
+type serviceExportsLister struct {
+	indexer cache.Indexer
+}
+
+// NewServiceExportsLister returns a new ServiceExportsLister.
+func NewServiceExportsLister(indexer cache.Indexer) ServiceExportsLister {
+	return &serviceExportsLister{indexer: indexer}
+}
+
+// List lists all ServiceExportses in the indexer.
+func (s *serviceExportsLister) List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error) {
+	err = cache.ListAll(s.indexer, selector, func(m interface{}) {
+		ret = append(ret, m.(*v1alpha1.ServiceExports))
+	})
+	return ret, err
+}
+
+// ServiceExportses returns an object that can list and get ServiceExportses.
+func (s *serviceExportsLister) ServiceExportses(namespace string) ServiceExportsNamespaceLister {
+	return serviceExportsNamespaceLister{indexer: s.indexer, namespace: namespace}
+}
+
+// ServiceExportsNamespaceLister helps list and get ServiceExportses.
+// All objects returned here must be treated as read-only.
+type ServiceExportsNamespaceLister interface {
+	// List lists all ServiceExportses in the indexer for a given namespace.
+	// Objects returned here must be treated as read-only.
+	List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error)
+	// Get retrieves the ServiceExports from the indexer for a given namespace and name.
+	// Objects returned here must be treated as read-only.
+	Get(name string) (*v1alpha1.ServiceExports, error)
+	ServiceExportsNamespaceListerExpansion
+}
+
+// serviceExportsNamespaceLister implements the ServiceExportsNamespaceLister
+// interface.
+type serviceExportsNamespaceLister struct {
+	indexer   cache.Indexer
+	namespace string
+}
+
+// List lists all ServiceExportses in the indexer for a given namespace.
+func (s serviceExportsNamespaceLister) List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error) {
+	err = cache.ListAllByNamespace(s.indexer, s.namespace, selector, func(m interface{}) {
+		ret = append(ret, m.(*v1alpha1.ServiceExports))
+	})
+	return ret, err
+}
+
+// Get retrieves the ServiceExports from the indexer for a given namespace and name.
+func (s serviceExportsNamespaceLister) Get(name string) (*v1alpha1.ServiceExports, error) {
+	obj, exists, err := s.indexer.GetByKey(s.namespace + "/" + name)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		return nil, errors.NewNotFound(v1alpha1.Resource("serviceexports"), name)
+	}
+	return obj.(*v1alpha1.ServiceExports), nil
+}

client/versioned/typed/core/v1alpha1/fake/fake_core_client.go
@@ -30,6 +30,10 @@ func (c *FakeCoreV1alpha1) MeshFederations(namespace string) v1alpha1.MeshFedera
 	return &FakeMeshFederations{c, namespace}
 }
 
+func (c *FakeCoreV1alpha1) ServiceExportses(namespace string) v1alpha1.ServiceExportsInterface {
+	return &FakeServiceExportses{c, namespace}
+}
+
 func (c *FakeCoreV1alpha1) ServiceMeshExtensions(namespace string) v1alpha1.ServiceMeshExtensionInterface {
 	return &FakeServiceMeshExtensions{c, namespace}
 }

maistra/api

client/informers/externalversions/core/v1alpha1/interface.go
@@ -24,6 +24,8 @@ import (
 type Interface interface {
 	// MeshFederations returns a MeshFederationInformer.
 	MeshFederations() MeshFederationInformer
+	// ServiceExports returns a ServiceExportsInformer.
+	ServiceExports() ServiceExportsInformer
 	// ServiceMeshExtensions returns a ServiceMeshExtensionInformer.
 	ServiceMeshExtensions() ServiceMeshExtensionInformer
 }
@@ -42,6 +44,11 @@ func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakList
 // MeshFederations returns a MeshFederationInformer.
 func (v *version) MeshFederations() MeshFederationInformer {
 	return &meshFederationInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
+}
+
+// ServiceExports returns a ServiceExportsInformer.
+func (v *version) ServiceExports() ServiceExportsInformer {
+	return &serviceExportsInformer{factory: v.factory, namespace: v.namespace, tweakListOptions: v.tweakListOptions}
 }
 
 // ServiceMeshExtensions returns a ServiceMeshExtensionInformer.

client/informers/externalversions/core/v1alpha1/serviceexports.go
@@ -1,0 +1,88 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by main. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	"context"
+	time "time"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	watch "k8s.io/apimachinery/pkg/watch"
+	cache "k8s.io/client-go/tools/cache"
+	internalinterfaces "maistra.io/api/client/informers/externalversions/internalinterfaces"
+	v1alpha1 "maistra.io/api/client/listers/core/v1alpha1"
+	versioned "maistra.io/api/client/versioned"
+	corev1alpha1 "maistra.io/api/core/v1alpha1"
+)
+
+// ServiceExportsInformer provides access to a shared informer and lister for
+// ServiceExports.
+type ServiceExportsInformer interface {
+	Informer() cache.SharedIndexInformer
+	Lister() v1alpha1.ServiceExportsLister
+}
+
+type serviceExportsInformer struct {
+	factory          internalinterfaces.SharedInformerFactory
+	tweakListOptions internalinterfaces.TweakListOptionsFunc
+	namespace        string
+}
+
+// NewServiceExportsInformer constructs a new informer for ServiceExports type.
+// Always prefer using an informer factory to get a shared informer instead of getting an independent
+// one. This reduces memory footprint and number of connections to the server.
+func NewServiceExportsInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers) cache.SharedIndexInformer {
+	return NewFilteredServiceExportsInformer(client, namespace, resyncPeriod, indexers, nil)
+}
+
+// NewFilteredServiceExportsInformer constructs a new informer for ServiceExports type.
+// Always prefer using an informer factory to get a shared informer instead of getting an independent
+// one. This reduces memory footprint and number of connections to the server.
+func NewFilteredServiceExportsInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().ServiceExports(namespace).List(context.TODO(), options)
+			},
+			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.CoreV1alpha1().ServiceExports(namespace).Watch(context.TODO(), options)
+			},
+		},
+		&corev1alpha1.ServiceExports{},
+		resyncPeriod,
+		indexers,
+	)
+}
+
+func (f *serviceExportsInformer) defaultInformer(client versioned.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
+	return NewFilteredServiceExportsInformer(client, f.namespace, resyncPeriod, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc}, f.tweakListOptions)
+}
+
+func (f *serviceExportsInformer) Informer() cache.SharedIndexInformer {
+	return f.factory.InformerFor(&corev1alpha1.ServiceExports{}, f.defaultInformer)
+}
+
+func (f *serviceExportsInformer) Lister() v1alpha1.ServiceExportsLister {
+	return v1alpha1.NewServiceExportsLister(f.Informer().GetIndexer())
+}

client/informers/externalversions/generic.go
@@ -65,6 +65,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		// Group=core, Version=v1alpha1
 	case v1alpha1.SchemeGroupVersion.WithResource("meshfederations"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().MeshFederations().Informer()}, nil
+	case v1alpha1.SchemeGroupVersion.WithResource("serviceexports"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().ServiceExports().Informer()}, nil
 	case v1alpha1.SchemeGroupVersion.WithResource("servicemeshextensions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Core().V1alpha1().ServiceMeshExtensions().Informer()}, nil
 

client/listers/core/v1alpha1/serviceexports.go
@@ -1,0 +1,97 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Code generated by main. DO NOT EDIT.
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/tools/cache"
+	v1alpha1 "maistra.io/api/core/v1alpha1"
+)
+
+// ServiceExportsLister helps list ServiceExports.
+// All objects returned here must be treated as read-only.
+type ServiceExportsLister interface {
+	// List lists all ServiceExports in the indexer.
+	// Objects returned here must be treated as read-only.
+	List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error)
+	// ServiceExports returns an object that can list and get ServiceExports.
+	ServiceExports(namespace string) ServiceExportsNamespaceLister
+	ServiceExportsListerExpansion
+}
+
+// serviceExportsLister implements the ServiceExportsLister interface.
+type serviceExportsLister struct {
+	indexer cache.Indexer
+}
+
+// NewServiceExportsLister returns a new ServiceExportsLister.
+func NewServiceExportsLister(indexer cache.Indexer) ServiceExportsLister {
+	return &serviceExportsLister{indexer: indexer}
+}
+
+// List lists all ServiceExports in the indexer.
+func (s *serviceExportsLister) List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error) {
+	err = cache.ListAll(s.indexer, selector, func(m interface{}) {
+		ret = append(ret, m.(*v1alpha1.ServiceExports))
+	})
+	return ret, err
+}
+
+// ServiceExports returns an object that can list and get ServiceExports.
+func (s *serviceExportsLister) ServiceExports(namespace string) ServiceExportsNamespaceLister {
+	return serviceExportsNamespaceLister{indexer: s.indexer, namespace: namespace}
+}
+
+// ServiceExportsNamespaceLister helps list and get ServiceExports.
+// All objects returned here must be treated as read-only.
+type ServiceExportsNamespaceLister interface {
+	// List lists all ServiceExports in the indexer for a given namespace.
+	// Objects returned here must be treated as read-only.
+	List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error)
+	// Get retrieves the ServiceExports from the indexer for a given namespace and name.
+	// Objects returned here must be treated as read-only.
+	Get(name string) (*v1alpha1.ServiceExports, error)
+	ServiceExportsNamespaceListerExpansion
+}
+
+// serviceExportsNamespaceLister implements the ServiceExportsNamespaceLister
+// interface.
+type serviceExportsNamespaceLister struct {
+	indexer   cache.Indexer
+	namespace string
+}
+
+// List lists all ServiceExports in the indexer for a given namespace.
+func (s serviceExportsNamespaceLister) List(selector labels.Selector) (ret []*v1alpha1.ServiceExports, err error) {
+	err = cache.ListAllByNamespace(s.indexer, s.namespace, selector, func(m interface{}) {
+		ret = append(ret, m.(*v1alpha1.ServiceExports))
+	})
+	return ret, err
+}
+
+// Get retrieves the ServiceExports from the indexer for a given namespace and name.
+func (s serviceExportsNamespaceLister) Get(name string) (*v1alpha1.ServiceExports, error) {
+	obj, exists, err := s.indexer.GetByKey(s.namespace + "/" + name)
+	if err != nil {
+		return nil, err
+	}
+	if !exists {
+		return nil, errors.NewNotFound(v1alpha1.Resource("serviceexports"), name)
+	}
+	return obj.(*v1alpha1.ServiceExports), nil
+}

client/versioned/typed/core/v1alpha1/fake/fake_core_client.go
@@ -30,6 +30,10 @@ func (c *FakeCoreV1alpha1) MeshFederations(namespace string) v1alpha1.MeshFedera
 	return &FakeMeshFederations{c, namespace}
 }
 
+func (c *FakeCoreV1alpha1) ServiceExports(namespace string) v1alpha1.ServiceExportsInterface {
+	return &FakeServiceExports{c, namespace}
+}
+
 func (c *FakeCoreV1alpha1) ServiceMeshExtensions(namespace string) v1alpha1.ServiceMeshExtensionInterface {
 	return &FakeServiceMeshExtensions{c, namespace}
 }